146ed54b8ad4dba86775585dfd72d1e74b4510da
Anubhav Kamal <anubhavkamal@users.noreply.github.com>
2026-06-07 10:36:10 +0530

    Update Softmax function (Math Foundations 3/6)

--- foundations/softmax.py
+++ foundations/softmax.py
@@ -3,16 +3,10 @@ from numpy.typing import NDArray
 
 
 class Solution:
-    
-    def sigmoid(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
-        # z is a 1D NumPy array
-        # Formula: 1 / (1 + e^(-z))
-        # return np.round(your_answer, 5)
-        sig = 1 / (1 + np.exp(-1 * z))
-        return np.round(sig, 5)
+    def softmax(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
+        z = z - np.max(z)
+        exp_z = np.exp(z)
+        return np.round(exp_z / np.sum(exp_z), 4)
+        # for i in z:
 
-    def relu(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
-        # z is a 1D NumPy array
-        # Formula: max(0, z) element-wise
-
-        return np.maximum(0, z)
+        #pass
